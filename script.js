9a537540c7b9b4f04735c64952e38166ce34d699
Artem Gladyshev <dlya-igor-artem@yandex.ru>
2025-03-10 23:20:34 +0300

    'lab1'

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-// Получаем кнопку по её ID
 const changeBackgroundBtn = document.getElementById('changeBackgroundBtn');
 
-// Изначальный цвет фона
 let isDefaultBackground = true;
 
-// Функция для смены фона
 function changeBackground() {
     if (isDefaultBackground) {
-        // Меняем фон на другой цвет
-        document.body.style.backgroundColor = '#666'; // Темный фон
+        document.body.style.backgroundColor = '#666';
     } else {
-        // Возвращаем исходный цвет
-        document.body.style.backgroundColor = '#f0f0f0'; // Светлый фон
+        document.body.style.backgroundColor = '#f0f0f0';
     }
-    // Переключаем состояние
     isDefaultBackground = !isDefaultBackground;
 }
 
-// Добавляем обработчик события на кнопку
 changeBackgroundBtn.addEventListener('click', changeBackground);
